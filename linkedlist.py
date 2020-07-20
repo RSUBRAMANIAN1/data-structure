6dace77b9bcaaf2692f520418935a28e58c08765
RSUBRAMANIAN1 <r.saiprasad99@gmail.com>
2020-07-20 18:40:42 +0530

    Linked list in python
    Working create node and print node functions

--- linkedlist.py
+++ linkedlist.py
@@ -1,2 +1,35 @@
+class Node:
 
-print("Hello world")
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+
+    def createnode(self,no):
+        if(self.head == None):
+            val = input()
+            self.head = Node(val)
+        else:
+            for i in range(no-1):
+                val = input()
+                self.next = Node(val)
+                self = self.next
+
+    def printnode(self):
+        temp = self.head
+        while(temp):
+            print(temp.data)
+            temp = temp.next
+
+
+if __name__ == "__main__":
+    llist = LinkedList()
+
+    n = int(input())
+    llist.createnode(n)
+    llist.printnode()
